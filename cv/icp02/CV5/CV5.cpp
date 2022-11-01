6f421e01ebdc508c33795814dc8bab3835f4a31a
vencoca <vaclav.kesler@gmail.com>
2022-11-01 19:14:49 +0100

    Update

--- cv/icp02/CV5/CV5.cpp
+++ cv/icp02/CV5/CV5.cpp
@@ -1,33 +1,37 @@
-// -------------------------------------------- BASIC C++ ------------------------------------------------
+﻿// -------------------------------------------- BASIC C++ ------------------------------------------------------
 #include <iostream>
 #include <chrono>
 #include <numeric>
 #include <thread>
-// ----------------------------------------- INCLUDE GRAPHIC ---------------------------------------------
+// ----------------------------------------- INCLUDE GRAPHIC ---------------------------------------------------
 #include <opencv2\opencv.hpp> // OpenCV
 #include <GL/glew.h> // OpenGL Extension Wrangler
 #include <GL/wglew.h> //WGLEW = Windows GL Extension Wrangler (change for different platform) 
 #include <GLFW/glfw3.h> // GLFW toolkit
 #include <glm/glm.hpp> // OpenGL math
-// ------------------------------------------- INIT DECLARE -----------------------------------------------
+// ------------------------------------------- INIT DECLARE ----------------------------------------------------
 static void init_opengl(void);
 static void init_glfw(void);
 static void finalize(int code);
-// ----------------------------------------- CALLBACKS DECLARE ------------------------------------------
+// ----------------------------------------- CALLBACKS DECLARE -------------------------------------------------
 void set_all_callbacks();
 void error_callback(int error, const char* description);
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 void fbsize_callback(GLFWwindow* window, int width, int height);
-// -------------------------------------------- STRUCTURES --------------------------------------------------
+void cursor_position_callback(GLFWwindow* window, double xpos, double ypos);
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
+// -------------------------------------------- STRUCTURES -----------------------------------------------------
 typedef struct s_globals {
     cv::VideoCapture capture;
     GLFWwindow* window;
     int width;
     int height;
+    double mouse_xpos;
+    double mouse_ypos;
     double app_start_time;
 } s_globals;
 s_globals globals;
-
+// -------------------------------------------- MAIN -----------------------------------------------------------
 int main() {
     init_opengl();
     set_all_callbacks();
@@ -37,12 +41,13 @@ int main() {
     finalize(1);
     return(EXIT_SUCCESS);
 }
-
 //----------------------------------------------- CALLBACKS -----------------------------------------------------
 void set_all_callbacks() {
     glfwSetErrorCallback(error_callback);
     glfwSetFramebufferSizeCallback(globals.window, fbsize_callback);
     glfwSetKeyCallback(globals.window, key_callback);
+    glfwSetCursorPosCallback(globals.window, cursor_position_callback);
+    glfwSetMouseButtonCallback(globals.window, mouse_button_callback);
 }
 
 void error_callback(int error, const char* description)
@@ -54,12 +59,29 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GLFW_TRUE);
+    if (key == GLFW_KEY_W && action == GLFW_PRESS)
+        std::cout << "W" << "\n";
+    if (key == GLFW_KEY_A && action == GLFW_PRESS)
+        std::cout << "A" << "\n";
+    if (key == GLFW_KEY_S && action == GLFW_PRESS)
+        std::cout << "S" << "\n";
+    if (key == GLFW_KEY_D && action == GLFW_PRESS)
+        std::cout << "D" << "\n";
+    if (key == GLFW_KEY_UP && action == GLFW_PRESS)
+        std::cout << "UP" << "\n";
+    if (key == GLFW_KEY_DOWN && action == GLFW_PRESS)
+        std::cout << "DOWN" << "\n";
+    if (key == GLFW_KEY_LEFT && action == GLFW_PRESS)
+        std::cout << "LEFT" << "\n";
+    if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS)
+        std::cout << "RIGHT" << "\n";
 }
 
 void fbsize_callback(GLFWwindow* window, int width, int height)
 {
     globals.width = width;
     globals.height = height;
+    std::cout << "Height: " << height << " Width: " << width << "\n";
 
     glMatrixMode(GL_PROJECTION);				// set projection matrix for following transformations
     glLoadIdentity();							// clear all transformations
@@ -69,8 +91,18 @@ void fbsize_callback(GLFWwindow* window, int width, int height)
     glViewport(0, 0, width, height);			// set visible area
 }
 
+void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    globals.mouse_xpos = xpos;
+    globals.mouse_ypos = ypos;
+}
 
-//----------------------------------------------- INIT OPENGL -----------------------------------------------------
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods) {
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+        std::cout << "xpos: " << globals.mouse_xpos << " ypos: " << globals.mouse_ypos << "\n";
+}
+
+//----------------------------------------------- INIT OPENGL ----------------------------------------------------
 static void init_opengl(void)
 {
     init_glfw();
